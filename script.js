64bad012f10e49b78aff0a3c297511162b37e302
Moritz <moritzmaximilian@gmx.net>
2023-07-14 20:01:01 +0200

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+const rentalNav = document.querySelector("#RentalNav");
+const navMenu = document.querySelector("#NavMenu");
+
+console.log(rentalNav);
+
+function navOpenRental() {
+    navMenu.classList.add('open');
+}
