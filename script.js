780c2933e19a8043dc51775cf43f0514bd63ed73
Moritz <moritzmaximilian@gmx.net>
2023-09-21 17:37:52 +0200

    .

--- script.js
+++ script.js
@@ -1,65 +1,77 @@
-var currentPage = "home";
+var currentPage = sessionStorage.getItem("currentPage");
 
 const NAVRENTAL = document.getElementById("NAVRENTAL");
 const NAVDESIGN = document.getElementById("NAVDESIGN");
 const NAVCONSULTING = document.getElementById("NAVCONSULTING");
+const NAVEVENTS = document.getElementById("NAVEVENTS");
 
+/* ----- check current NavLink ----- */
 function checkNav() {
-
-    console.log(currentPage + "test");
 
     switch (currentPage) {
 
-        case 'rental':
+        case "rental":
             NAVRENTAL.classList.add('navLinkActive');
             NAVDESIGN.classList.remove('navLinkActive');
             NAVCONSULTING.classList.remove('navLinkActive');
-            console.log("nav rental");
+            NAVEVENTS.classList.remove('navLinkActive');
             break;
 
-        case 'design':
+        case "design":
             NAVRENTAL.classList.remove('navLinkActive');
             NAVDESIGN.classList.add('navLinkActive');
             NAVCONSULTING.classList.remove('navLinkActive');
-            console.log("nav design");
+            NAVEVENTS.classList.remove('navLinkActive');
             break;
 
-        case 'consulting':
+        case "consulting":
             NAVRENTAL.classList.remove('navLinkActive');
             NAVDESIGN.classList.remove('navLinkActive');
             NAVCONSULTING.classList.add('navLinkActive');
-            console.log("nav consulting")
+            NAVEVENTS.classList.remove('navLinkActive');
+            break;
+
+        case "events":
+            NAVRENTAL.classList.remove('navLinkActive');
+            NAVDESIGN.classList.remove('navLinkActive');
+            NAVCONSULTING.classList.remove('navLinkActive');
+            NAVEVENTS.classList.add('navLinkActive');
             break;
 
         default:
             break;
     }
-
-    console.log("easy");
 }
+
+checkNav();
 
 function openPage(page) {
 
     switch (page) {
 
-        case "home":
+        case 'home':
             sessionStorage.setItem("currentPage", "home");
-            window.open("index.html", "_self");
+            window.open("index.html", "_self")
             break;
 
-        case "rental":
+        case 'rental':
             sessionStorage.setItem("currentPage", "rental");
             window.open("rental.html", "_self");
             break;
 
-        case "design":
+        case 'design':
             sessionStorage.setItem("currentPage", "design");
             window.open("design.html", "_self");
             break;
 
-        case "consulting":
+        case 'consulting':
             sessionStorage.setItem("currentPage", "consulting");
             window.open("consulting.html", "_self");
+            break;
+
+        case 'events':
+            sessionStorage.setItem("currentPage", "events");
+            window.open("events.html", "_self");
             break;
 
         default:
