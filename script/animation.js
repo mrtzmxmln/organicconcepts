2678e2265ae08b8dbf2b29caef5a08b9c4e16c29
mrtzmxmln <moritzmaximilian@gmx.net>
2025-03-09 18:02:56 +0100

    design changes & imprint update

---
+++ script/animation.js
@@ -0,0 +1,20 @@
+window.addEventListener("resize", navDisplay);
+
+var navLinkBox = document.getElementById("NAVLINKBOX");
+
+function openNav() {
+    if (navLinkBox.style.display === "flex") {
+        navLinkBox.style.display = "none";
+    } else {
+        navLinkBox.style.display = "flex";
+    }
+}
+
+function navDisplay() {
+    var w = window.innerWidth;
+    if(w >= 800) {
+        navLinkBox.style.display = "flex";
+    } else if (w < 800) {
+        navLinkBox.style.display = "none";
+    }
+}
