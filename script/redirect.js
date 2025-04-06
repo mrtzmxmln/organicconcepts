e689be0e732e8b090bc1ac4ded53d8514804a59e
mrtzmxmln <moritzmaximilian@gmx.net>
2025-04-06 14:24:52 +0200

    fixed urls

--- script/redirect.js
+++ script/redirect.js
@@ -1,11 +1,11 @@
 function openHome() {
-    window.open("https://organicconcepts.de").focus();
+    window.open("https://organicconcepts.de", "_self").focus();
 }
 
 function openPool() {
-    window.location.href="./pool.html"
+    window.location.href="./pool.html";
 }
 
 function openInstagram() {
-    window.open("https://instagram.com/organicconcepts", '_blank').focus();
+    window.open("https://instagram.com/organicconcepts", "_blank").focus();
 }
