5e0f8724c29285b72845eeb518bdbcda9f52bb90
mrtzmxmln <moritzmaximilian@gmx.net>
2025-04-20 18:50:29 +0200

    .

--- script/animation.js
+++ script/animation.js
@@ -1,41 +1,43 @@
 window.addEventListener("resize", navDisplay);
 
 function openNav() {
-    const navLinkBox = document.getElementById("NAVLINKBOX");
-    if (!navLinkBox) return;
-  
-    navLinkBox.style.display = (navLinkBox.style.display === "flex") ? "none" : "flex";
-  }
-  
-  function navDisplay() {
-    const navLinkBox = document.getElementById("NAVLINKBOX");
-    if (!navLinkBox) return;
-  
-    var w = window.innerWidth;
-    if (w >= 600) {
-      navLinkBox.style.display = "flex";
-    } else {
-      navLinkBox.style.display = "none";
-    }
-  }
+  const navLinkBox = document.getElementById("NAVLINKBOX");
+  if (!navLinkBox) return;
+
+  navLinkBox.style.display =
+    navLinkBox.style.display === "flex" ? "none" : "flex";
+}
 
 function navDisplay() {
-    var w = window.innerWidth;
-    if(w >= 600) {
-        navLinkBox.style.display = "flex";
-    } else if (w < 600) {
-        navLinkBox.style.display = "none";
-    }
+  const navLinkBox = document.getElementById("NAVLINKBOX");
+  if (!navLinkBox) return;
+
+  var w = window.innerWidth;
+  if (w >= 600) {
+    navLinkBox.style.display = "flex";
+  } else {
+    navLinkBox.style.display = "none";
+  }
+}
+
+function navDisplay() {
+  var w = window.innerWidth;
+  if (w >= 600) {
+    navLinkBox.style.display = "flex";
+  } else if (w < 600) {
+    navLinkBox.style.display = "none";
+  }
 }
 
 function openCart() {
-    document.getElementById('cartSidebar').classList.add('active');
-    document.getElementById('cartOverlay').classList.add('active');
-    document.body.style.overflow = 'hidden';
+  renderCart(); // 👉 jedes Mal neu rendern
+  document.getElementById("cartSidebar").classList.add("active");
+  document.getElementById("cartOverlay").classList.add("active");
+  document.body.style.overflow = "hidden";
 }
 
 function closeCart() {
-    document.getElementById('cartSidebar').classList.remove('active');
-    document.getElementById('cartOverlay').classList.remove('active');
-    document.body.style.overflow = 'auto';
+  document.getElementById("cartSidebar").classList.remove("active");
+  document.getElementById("cartOverlay").classList.remove("active");
+  document.body.style.overflow = "auto";
 }
