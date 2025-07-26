d4a6e52a104b256448afbef26239891c5b723fb9
Moritz <moritzmaximilian@gmx.net>
2025-07-26 17:07:01 +0200

    new version

--- script/redirect.js
+++ script/redirect.js
@@ -6,10 +6,22 @@ function openPool() {
   window.location.href = "../pool/";
 }
 
+function openConsulting() {
+  window.location.href = "../consulting/";
+}
+
+function openCal() {
+  window.open("https://cal.com/oc-moritz/kennenlernen", "_blank").focus();
+}
+
 function openInstagram() {
   window.open("https://instagram.com/organicconcepts", "_blank").focus();
 }
 
 function openDiscord() {
   window.open("http://discord.organicconcepts.de", "_blank").focus();
+}
+
+function openEvents() {
+  window.open("http://tickets.organicconcepts.de", "_blank").focus();
 }
